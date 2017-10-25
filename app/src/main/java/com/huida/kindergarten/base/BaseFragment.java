17b0e23fbc2710d0ddc6864d256dac627555e1a9
JiangYu <you@example.com>
2017-10-25 08:48:09 +0800

    10.25

--- app/src/main/java/com/huida/kindergarten/base/BaseFragment.java
+++ app/src/main/java/com/huida/kindergarten/base/BaseFragment.java
@@ -3,9 +3,12 @@ package com.huida.kindergarten.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Scout
@@ -15,6 +18,14 @@ import android.view.ViewGroup;
 abstract public class BaseFragment extends Fragment {
 
     private static final String TAG = "BaseFragment";
+
+    /**上下文*/
+    protected FragmentActivity mActivity;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = getActivity();
+    }
 
     @Nullable
     @Override
--- app/src/main/java/com/huida/kindergarten/fragment/ClassFragment.java
+++ app/src/main/java/com/huida/kindergarten/fragment/ClassFragment.java
@@ -1,15 +1,41 @@
 package com.huida.kindergarten.fragment;
 
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.huida.kindergarten.R;
 import com.huida.kindergarten.base.BaseFragment;
+import com.huida.kindergarten.view.XCRoundRectImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
- * Created by Scout
- * Created on 2017/10/24 14:03.
+ * Created by 81521 on 2017/10/24.
+ * 班级Fragment
  */
+
 public class ClassFragment extends BaseFragment {
+
+    private ImageButton mIbClassMsg;
+    private XCRoundRectImageView mIvHead;
+    private TextView mTvBabyName;
+    private TextView mTvBabyClass;
+    private RecyclerView mRvMoudle;
+    private RecyclerView mRvRanking;
+    private RecyclerView mRvShuoshuo;
+
+    String[] names = {"园所通知","园所缴费","视频观看","园所食谱","宝宝出勤","亲子作业","班级相册","教学计划"};
+    int icons[] = {R.mipmap.home_notice,R.mipmap.home_payment,R.mipmap.home_video,R.mipmap.home_cook
+            ,R.mipmap.home_kq,R.mipmap.home_homework,R.mipmap.home_album,R.mipmap.home_teachplan};
 
     @Override
     protected int getContentView() {
@@ -18,11 +44,91 @@ public class ClassFragment extends BaseFragment {
 
     @Override
     protected void initViews(View view) {
+        mRvMoudle = view.findViewById(R.id.rv_moudle);
+        mIbClassMsg = view.findViewById(R.id.ib_class_msg);
+        mIvHead = view.findViewById(R.id.iv_head);
+        mTvBabyName = view.findViewById(R.id.tv_baby_name);
+        mTvBabyClass = view.findViewById(R.id.tv_baby_class);
+        mRvMoudle = view.findViewById(R.id.rv_moudle);
+        mRvRanking = view.findViewById(R.id.rv_ranking);
+        mRvShuoshuo = view.findViewById(R.id.rv_shuoshuo);
+        initMoudles();
+    }
+
+    private void initMoudles() {
+        mRvMoudle.setLayoutManager(new GridLayoutManager(mActivity,4));
+        mRvMoudle.setAdapter(new ClassMoudleAdapter());
+
 
     }
 
     @Override
     protected void initListeners() {
 
+    }
+
+    /**
+     * 八大模块的适配器
+     */
+    class ClassMoudleAdapter extends RecyclerView.Adapter<ClassMoudleAdapter.ClassMoudleViewHolder>{
+        @Override
+        public ClassMoudleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(mActivity).inflate(R.layout.item_class_moudle, parent, false);
+            return new ClassMoudleViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ClassMoudleViewHolder holder, final int position) {
+            holder.iv_icon.setImageResource(icons[position]);
+            holder.tv_name.setText(names[position]);
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //点击事件
+                    onMoudleItemClick(position);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return names.length;
+        }
+
+        class ClassMoudleViewHolder extends RecyclerView.ViewHolder{
+            ImageView iv_icon;
+            TextView tv_name;
+            public ClassMoudleViewHolder(View itemView) {
+                super(itemView);
+                iv_icon = itemView.findViewById(R.id.class_item_icon);
+                tv_name = itemView.findViewById(R.id.class_item_name);
+            }
+        }
+    }
+
+    /**
+     * 模块的条目点击事件
+     * @param position
+     */
+    private void onMoudleItemClick(int position) {
+        switch (position){
+            case 0://园所通知
+                break;
+            case 1://园所缴费
+                break;
+            case 2://视频观看
+                break;
+            case 3://园所食谱
+                break;
+            case 4://宝宝出勤
+                break;
+            case 5://亲子作业
+                break;
+            case 6://班级相册
+                break;
+            case 7://教学计划
+                break;
+        }
     }
 }
